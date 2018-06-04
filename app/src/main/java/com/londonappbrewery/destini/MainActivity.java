cad281a066758ae2def0aaae17de692a2c50c97d
AMeikle99 <aidenmeikle@ymail.com>
2018-06-04 15:36:32 +0100

    Finalised Project

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -12,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     // TODO: Steps 4 & 8 - Declare member variables here:
     static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    private int mStoryIndex;
+
+    TextView questionTextView;
+    Button btnTop;
+    Button btnBottom;
 
 
     @Override
@@ -19,15 +24,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView questionTextView = findViewById(R.id.storyTextView);
-        final Button btnTop = findViewById(R.id.buttonTop);
-        final Button btnBottom = findViewById(R.id.buttonBottom);
+        questionTextView = findViewById(R.id.storyTextView);
+        btnTop = findViewById(R.id.buttonTop);
+        btnBottom = findViewById(R.id.buttonBottom);
+
+        mStoryIndex = 1;
+        updateStory();
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
         btnTop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d(LOG_TAG,"Top Button Pressed!");
+                if(mStoryIndex == 1 || mStoryIndex == 2){
+                    mStoryIndex = 3;
+                }else{
+                    mStoryIndex = 6;
+                }
+                updateStory();
             }
         });
 
@@ -38,6 +52,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(LOG_TAG, "Bottom Button Pressed!");
+                if(mStoryIndex == 1){
+                    mStoryIndex = 2;
+                }else if(mStoryIndex == 2){
+                    mStoryIndex = 4;
+                }else if(mStoryIndex == 3){
+                    mStoryIndex = 5;
+                }
+
+                updateStory();
             }
         });
 
@@ -45,5 +68,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateStory(){
 
+        switch(mStoryIndex){
+            case 1:
+                questionTextView.setText(R.string.T1_Story);
+                btnTop.setText(R.string.T1_Ans1);
+                btnBottom.setText(R.string.T1_Ans2);
+                break;
+            case 2:
+                questionTextView.setText(R.string.T2_Story);
+                btnTop.setText(R.string.T2_Ans1);
+                btnBottom.setText(R.string.T2_Ans2);
+                break;
+            case 3:
+                questionTextView.setText(R.string.T3_Story);
+                btnTop.setText(R.string.T3_Ans1);
+                btnBottom.setText(R.string.T3_Ans2);
+                break;
+            case 4:
+                questionTextView.setText(R.string.T4_End);
+                btnTop.setVisibility(View.GONE);
+                btnBottom.setVisibility(View.GONE);
+                break;
+            case 5:
+                questionTextView.setText(R.string.T5_End);
+                btnTop.setVisibility(View.GONE);
+                btnBottom.setVisibility(View.GONE);
+                break;
+            case 6:
+                questionTextView.setText(R.string.T6_End);
+                btnTop.setVisibility(View.GONE);
+                btnBottom.setVisibility(View.GONE);
+                break;
+        }
     }
 }
